e36b9339cbd95fa54fb551d935cbb47146638261
Mina Ayman <mtrmena176@gmail.com>
2025-03-13 20:26:06 +0200

    Register Note Model Adapter

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -7,14 +7,14 @@ class NoteModel extends HiveObject {
   @HiveField(0)
   final String title;
   @HiveField(1)
-  final String subTitle;
+  final String content;
   @HiveField(2)
   final String date;
   @HiveField(3)
   final int color;
   NoteModel({
     required this.title,
-    required this.subTitle,
+    required this.content,
     required this.date,
     required this.color,
   });
