cd37f7160882c6dbe4afe4e8174aedbba92f74e0
Mina Ayman <mtrmena176@gmail.com>
2025-03-13 20:46:00 +0200

    create addnote method and providd the cubit

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,13 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app1/models/note.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-
-
-
-  addNote(){}
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>('notes');
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      AddNoteFailure(e.toString());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:notes_app1/constants.dart';
+import 'package:notes_app1/cubits/cubit/add_note_cubit.dart';
 import 'package:notes_app1/models/note.dart';
 import 'package:notes_app1/views/edit_note.dart';
 import 'package:notes_app1/views/notes_view.dart';
@@ -17,17 +19,22 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: NotesView.routeName,
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        NotesView.routeName: (context) => const NotesView(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        EditNoteView.routeName: (context) => const EditNoteView(),
-      },
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      debugShowCheckedModeBanner: false,
-      home: NotesView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        initialRoute: NotesView.routeName,
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          NotesView.routeName: (context) => const NotesView(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          EditNoteView.routeName: (context) => const EditNoteView(),
+        },
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        debugShowCheckedModeBanner: false,
+        home: NotesView(),
+      ),
     );
   }
 }
